d751e381e63c88ee521acea65add0bae59c67d1e
Alex2574 <aleksandr.strela@yahoo.com>
2020-08-03 12:10:02 +0300

    Create project pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NewsPageComponent } from './news-page/news-page.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
+import { HubblePageComponent } from './hubble-page/hubble-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { LinkPageComponent } from './link-page/link-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    NewsPageComponent,
+    GalleryPageComponent,
+    HubblePageComponent,
+    AboutPageComponent,
+    LinkPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hubble-page/hubble-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hubble-page',
+  templateUrl: './hubble-page.component.html',
+  styleUrls: ['./hubble-page.component.scss']
+})
+export class HubblePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
