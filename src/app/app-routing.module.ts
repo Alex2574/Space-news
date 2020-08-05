ca6b61434a70a64e4b3e78c324e633eacfc8e3c8
Alex2574 <aleksandr.strela@yahoo.com>
2020-08-05 12:42:32 +0300

    Added links,to navigate page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
+import { HubblePageComponent } from './hubble-page/hubble-page.component';
+import { LinkPageComponent } from './link-page/link-page.component';
+import { NewsPageComponent } from './news-page/news-page.component';
+import { SentEmailComponent } from './sent-email/sent-email.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NavbarComponent,
+  },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'gallery', component: GalleryPageComponent },
+  { path: 'hubble', component: HubblePageComponent },
+  { path: 'link', component: LinkPageComponent },
+  { path: 'news', component: NewsPageComponent },
+  { path: 'email', component: SentEmailComponent },
+  { path: 'admin', loadChildren: './admin/admin.module#AdminModule' },
+  { path: '**', redirectTo: '/' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
